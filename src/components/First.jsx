df690079e1f90db497c40a02c57381fff882d877
Adwait Barkale <adwaitbarkale@gmail.com>
2024-12-03 14:47:54 +0530

    Reusable Components created and reused

---
+++ src/components/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const First = () => {
+  return(
+    <View>
+      <Text>This is a reusable Component exported from seperate folder.</Text>
+    </View>
+  )
+}
+
+export default First;
